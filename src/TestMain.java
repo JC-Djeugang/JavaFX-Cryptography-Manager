d7db92641262152d303b47a715381de37a704fff
Jean Claude Djeugang <djeugandrea@gmail.com>
2026-07-06 23:32:44 -0400

    Initial commit - JavaFX Cryptography Manager

---
+++ src/TestMain.java
@@ -0,0 +1,18 @@
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		CryptoManager test = new CryptoManager();
+		
+		//System.out.println( test.caesarEncryption("ABC", 96) );
+		
+		//System.out.println( test.caesarDecryption(";,:;05.G(56;/,9G:;905.", 999) );
+		
+		System.out.println( test.caesarEncryption("HELLO!", 3));
+		//System.out.println( test.caesarDecryption("KHOOR!", 3));
+		
+		
+
+	}
+
+}
